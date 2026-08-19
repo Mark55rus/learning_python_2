d528bc5e6830670383734f06e6f01a42ad8db831
Mark55rus <Mark_55rus@mail.ru>
2026-08-19 23:19:07 +0600

    task_03.04.05-08 complete

---
+++ balakirev/lesson_03_strings_and_lists/03.04_special_characters_and_escaping/task_03.04.07.py
@@ -0,0 +1,8 @@
+# Подвиг 5. Используя raw-строки, задайте (объявите) строку, содержащую следующий путь к файлу:
+
+# C:\WINDOWS\System32\drivers\etc\hosts
+
+# Полученную строку выведите в консоль (на экран).
+
+txt = r'C:\WINDOWS\System32\drivers\etc\hosts'
+print(txt)
